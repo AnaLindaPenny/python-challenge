2e9102a7715e474098b362ed117ec6c78b644537
Ana Linda Penny <AnaLindaPenny@Macs-Mac-mini.local>
2021-03-17 03:30:43 -0400

    add Solved directory

---
+++ PyBank/Solved/main.py
@@ -0,0 +1,10 @@
+import os
+import csv
+
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+print(csvpath)
+
+with open(csvpath, 'r') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+
+    print(csv_reader)
