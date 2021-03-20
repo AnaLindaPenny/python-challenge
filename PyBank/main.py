e2ce2738836ed3f1514adcfd65cfaa9ecfb8d51b
Ana Linda Penny <AnaLindaPenny@Macs-Mac-mini.local>
2021-03-19 23:02:14 -0400

    add main.py file

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,11 +5,47 @@ csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 print(csvpath)
 
 with open(csvpath, 'r') as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csv_reader)
+    print(csvreader)
 
-    
+    headers = next(csvreader)
+
+    print(headers)
+
+total_months = 0
+total_profit_losses = 0
+previous_profit_losses = 0
+average_change = 0
+profit_losses_change = 0
+profit_losses_changes = []
+
+greatest_inc = ['', 0]
+greatest_dec = ['', 9999999999]
+profit_losses_changes = []
+
+inputFile = os.path.join('..', 'Resources', 'budget_data.csv')
+
+with open(inputFile) as FinancialAnalysisData:
+    reader = csv.DictReader(FinancialAnalysisData)
+
+    for row in csvreader:
+        total_months = total_months + 1
+        total_profit_losses = total_profit_losses + int(row['Profit/Losses'])
+
+        if (previous_profit_losses != 0):
+            profit_losses_losses_change = int(row['Profit/Losses']) - previous_profit_losses
+
+        previous_profit_losses = int(row['Profit/Losses'])
+    print(row)
+
+output_path = os.path.join('..', 'PyBank', 'Analysis', 'financial_analysis_data.txt')
+
+with open(output_path, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(['Date', 'Profit/Losses'])
+
+
 
 
 
